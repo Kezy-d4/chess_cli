e150dee749601f177c664d268d2662b959a77285
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-04-04 13:14:30 -0400

    refactor: simplify Position
    The class has been refactored to focus primarily on combining the state
    of a Board and AuxPosData to represent a chess position.

--- lib/chess/position.rb
+++ lib/chess/position.rb
@@ -2,38 +2,22 @@
 
 module Chess
   # A chess position
-  class Position # rubocop:disable Metrics/ClassLength
-    using HashExtensions
+  class Position
+    attr_reader :board, :aux_pos_data
 
     # @param board [Board]
     # @param aux_pos_data [AuxPosData]
-    # @param player_white [Player] the Player playing white
-    # @param player_black [Player] the Player playing black
-    # @param log [Log]
-    def initialize(board, aux_pos_data, player_white, player_black, log)
+    def initialize(board, aux_pos_data)
       @board = board
       @aux_pos_data = aux_pos_data
-      @player_white = player_white
-      @player_black = player_black
-      @log = log
     end
 
     class << self
       # @param fen_parser [FENParser]
-      # @param player_white_name [String]
-      # @param player_black_name [String]
-      # @param log [Log]
-      def from_fen_parser(
-        fen_parser,
-        player_white_name = 'w',
-        player_black_name = 'b',
-        log = Log.new
-      )
+      def from_fen_parser(fen_parser)
         board = Board.from_fen_parser(fen_parser)
         aux_pos_data = AuxPosData.from_fen_parser(fen_parser)
-        player_white = Player.new(player_white_name, :white)
-        player_black = Player.new(player_black_name, :black)
-        new(board, aux_pos_data, player_white, player_black, log)
+        new(board, aux_pos_data)
       end
     end
 
@@ -41,275 +25,8 @@ module Chess
       "#{@board.to_partial_fen} #{@aux_pos_data.to_partial_fen}"
     end
 
-    def move(source_coord, destination_coord)
-      piece = @board.square_at(source_coord).occupant
-      @board.empty_at(source_coord)
-      @board.update_at(destination_coord, piece)
-    end
-
-    def check?
-      to_attacked_destinations_by(to_inactive_player).include?(to_king_source(to_active_player))
-    end
-
-    def checkmate?
-      check? && !any_legal_moves_available_to?(to_active_player)
-    end
-
-    def stalemate?
-      !check? && !any_legal_moves_available_to?(to_active_player)
-    end
-
-    def draw_by_fifty_move_rule?
-      @aux_pos_data.fifty_move_rule_satisfied?
-    end
-
-    def draw_by_threefold_repetition_rule?
-      @log.threefold_repetition_rule_satisfied?
-    end
-
-    def over?
-      checkmate? || stalemate? || draw_by_fifty_move_rule? || draw_by_threefold_repetition_rule?
-    end
-
-    def legal_move?(source_coord, destination_coord)
-      legal_source?(source_coord) && to_legal_destinations_from(source_coord).include?(destination_coord)
-    end
-
-    def to_legal_destinations_from(coord)
-      to_legal_controlled_destinations_from(coord) + to_legal_attacked_destinations_from(coord)
-    end
-
-    def legal_source?(coord)
-      to_player_sources(to_active_player).include?(coord)
-    end
-
-    def select_source(coord)
-      @log.update_metadata(
-        [:current_source, coord],
-        [:currently_controlled, to_legal_controlled_destinations_from(coord)],
-        [:currently_attacked, to_legal_attacked_destinations_from(coord)]
-      )
-    end
-
-    def deselect_source
-      @log.reset_metadata(:current_source, :currently_controlled, :currently_attacked)
-    end
-
-    def to_active_player
-      @aux_pos_data.white_has_the_move? ? @player_white : @player_black
-    end
-
-    def to_inactive_player
-      @aux_pos_data.white_has_the_move? ? @player_black : @player_white
-    end
-
-    def swap_active_player
-      @aux_pos_data.swap_active_color
-    end
-
-    def to_player_sources(player)
-      to_player_associations(player).keys
-    end
-
-    def previous_move_eligible_for_promotion?
-      return false unless previous_pawn_move?
-
-      coord = dump_log[:metadata][:previous_destination]
-      pawn = @board.square_at(coord).occupant
-      pawn.eligible_for_promotion?(coord)
-    end
-
-    def promote_previously_moved_pawn(piece)
-      pawn = @board.square_at(dump_log[:metadata][:previous_destination]).occupant
-      pawn.promote(piece)
-    end
-
-    def update_half_move_clock_after_move
-      if previous_capture? || previous_pawn_move?
-        @aux_pos_data.reset_half_move_clock
-      else
-        @aux_pos_data.increment_half_move_clock
-      end
-    end
-
-    def update_full_move_number_before_player_swap
-      @aux_pos_data.increment_full_move_number if to_active_player == @player_black
-    end
-
-    def update_en_passant_target_after_move
-      if previous_double_pawn_push?
-        target = dump_log[:metadata][:previous_destination].to_adjacency(0, -1).to_s
-        @aux_pos_data.update_en_passant_target(target)
-      else
-        @aux_pos_data.reset_en_passant_target
-      end
-    end
-
-    def capture_en_passant_target_before_move(source_coord, destination_coord)
-      capture_coord = to_en_passant_capture_coord(source_coord, destination_coord)
-      @board.square_at(capture_coord).remove_occupant
-    end
-
-    def move_to_en_passant_capture?(source_coord, destination_coord)
-      @board.square_at(source_coord).occupant.is_a?(Pawn) &&
-        destination_coord.to_s == @aux_pos_data.access_en_passant_target
-    end
-
-    # rubocop:disable Metrics/MethodLength
-    def update_metadata_before_move(source_coord, destination_coord)
-      if @board.occupied_at?(destination_coord)
-        @log.update_metadata(
-          [:previous_capture, destination_coord],
-          [:previous_source, source_coord],
-          [:previous_destination, destination_coord]
-        )
-      elsif move_to_en_passant_capture?(source_coord, destination_coord)
-        @log.update_metadata(
-          [:previous_capture, to_en_passant_capture_coord(source_coord, destination_coord)],
-          [:previous_source, source_coord],
-          [:previous_destination, destination_coord]
-        )
-      elsif @board.unoccupied_at?(destination_coord)
-        @log.reset_metadata(:previous_capture)
-        @log.update_metadata(
-          [:previous_source, source_coord],
-          [:previous_destination, destination_coord]
-        )
-      end
-    end
-    # rubocop: enable all
-
-    def update_metadata_after_player_swap
-      if check?
-        @log.update_metadata([:checked_king, to_king_source(to_active_player)])
-      elsif !check?
-        @log.reset_metadata(:checked_king)
-      end
-    end
-
-    def push_to_fen_history
-      @log.push_fen(to_fen)
-    end
-
-    def to_board_ranks
-      @board.to_ranks
-    end
-
-    def dump_log
-      @log.dump
-    end
-
-    def to_s
-      <<~HEREDOC
-        FEN: #{to_fen}
-        Player playing white: #{@player_white}
-        Player playing black: #{@player_black}
-        Log:
-        #{@log}
-      HEREDOC
-    end
-
-    private
-
-    def any_legal_moves_available_to?(player)
-      to_legal_moves_available_to(player).any?
-    end
-
-    def to_legal_moves_available_to(player)
-      hash = to_player_sources(player).each_with_object({}) do |coord, hash|
-        hash[coord] = to_legal_destinations_from(coord)
-      end
-      hash.delete_empty_arr_vals
-    end
-
-    def move_would_leave_active_player_in_check?(source_coord, destination_coord)
-      clone = self.clone
-      if clone.move_to_en_passant_capture?(source_coord, destination_coord)
-        clone.capture_en_passant_target_before_move(source_coord, destination_coord)
-      end
-      clone.move(source_coord, destination_coord)
-      clone.check?
-    end
-
-    def to_legal_controlled_destinations_from(source_coord)
-      return [] unless legal_source?(source_coord)
-
-      to_controlled_destinations_from(source_coord).reject do |destination_coord|
-        destination_coord == to_king_source(to_inactive_player) ||
-          move_would_leave_active_player_in_check?(source_coord, destination_coord)
-      end
-    end
-
-    def to_legal_attacked_destinations_from(source_coord)
-      return [] unless legal_source?(source_coord)
-
-      to_attacked_destinations_from(source_coord).reject do |destination_coord|
-        destination_coord == to_king_source(to_inactive_player) ||
-          move_would_leave_active_player_in_check?(source_coord, destination_coord)
-      end
-    end
-
-    def to_controlled_destinations_from(coord)
-      @board.to_adjacent_controlled_coords_from(coord).values.flatten
-    end
-
-    def to_attacked_destinations_from(coord)
-      @board.to_adjacent_attacked_coords_from(
-        coord,
-        @aux_pos_data.access_en_passant_target
-      ).values.flatten
-    end
-
-    def to_possible_destinations_from(coord)
-      to_controlled_destinations_from(coord) + to_attacked_destinations_from(coord)
-    end
-
-    def to_attacked_destinations_by(player)
-      arr = to_player_sources(player).each_with_object([]) do |coord, arr|
-        arr << to_attacked_destinations_from(coord)
-      end
-      arr.flatten
-    end
-
-    def to_king_source(player)
-      to_player_sources(player).find do |coord|
-        @board.square_at(coord).occupant.instance_of?(King)
-      end
-    end
-
-    def to_player_associations(player)
-      @board.to_occupied_associations(player.color)
-    end
-
     def clone
       Marshal.load(Marshal.dump(self))
-    end
-
-    def previous_capture?
-      !!dump_log[:metadata][:previous_capture]
-    end
-
-    def previous_pawn_move?
-      piece = @board.square_at(dump_log[:metadata][:previous_destination]).occupant
-      piece.is_a?(Pawn)
-    end
-
-    def previous_double_pawn_push?
-      return false unless previous_pawn_move?
-
-      metadata = dump_log[:metadata]
-      previous_source = metadata[:previous_source]
-      previous_destination = metadata[:previous_destination]
-      previous_destination.rank - previous_source.rank == 2
-    end
-
-    def to_en_passant_capture_coord(source_coord, destination_coord)
-      source_pawn = @board.square_at(source_coord).occupant
-      if source_pawn.white?
-        destination_coord.to_adjacency(0, -1)
-      elsif source_pawn.black?
-        destination_coord.to_adjacency(0, 1)
-      end
     end
   end
 end
