12e62fca43c6fd9cdc648411d361500468b7ba2e
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-04-11 22:47:52 -0400

    feat: add MoveValidator

---
+++ lib/chess/move_validator.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Chess
+  # Validates moves in the context of a chess position
+  class MoveValidator
+    # @param position [Position]
+    def initialize(position)
+      @position = position
+    end
+
+    def legal_move?(source, destination)
+      legal_source?(source) && to_all_legal_destinations_from(source).include?(destination)
+    end
+
+    def legal_source?(source)
+      @position.to_all_sources(@position.to_active_color).include?(source)
+    end
+
+    # Returns a complete array of attacked destinations from a given legal source.
+    # The method rejects destinations that would leave the active color in check
+    # and includes any potential en passant attacks.
+    def to_legal_attacked_destinations_from(source)
+      arr = []
+      return arr unless legal_source?(source)
+
+      arr << SpecialMoveAnalyzer.new(@position).to_en_passant_destinations_from(source)
+      arr << @position.to_attacked_destinations_from(source)
+      arr.flatten.reject do |destination|
+        @position.move_would_leave_active_color_in_check?(source, destination)
+      end
+    end
+
+    # Returns a complete array of controlled destinations from a given legal source.
+    # The method rejects destinations that would leave the active color in check
+    # and includes any potential castling destinations.
+    def to_legal_controlled_destinations_from(source)
+      return [] unless legal_source?(source)
+
+      arr = @position.to_controlled_destinations_from(source).reject do |destination|
+        @position.move_would_leave_active_color_in_check?(source, destination)
+      end
+      (arr << SpecialMoveAnalyzer.new(@position).to_legal_castle_destinations_from(source)).flatten
+    end
+
+    private
+
+    def to_all_legal_destinations_from(source)
+      to_legal_attacked_destinations_from(source) + to_legal_controlled_destinations_from(source)
+    end
+  end
+end
