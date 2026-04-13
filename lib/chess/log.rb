32e8334af217c88c39b5a729c27bb306fd603b39
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-04-13 18:02:26 -0400

    feat: extend Log

--- lib/chess/log.rb
+++ lib/chess/log.rb
@@ -11,5 +11,20 @@ module Chess
       @metadata = metadata
       @fen_history = fen_history
     end
+
+    def threefold_repetition_rule_satisfied?
+      hash = Hash.new(0)
+      to_simplified_fen_history.each do |fen|
+        hash[fen] += 1
+        return true if hash[fen] == 3
+      end
+      false
+    end
+
+    private
+
+    def to_simplified_fen_history
+      @fen_history.map { |fen| fen.split[0..3].join(' ') }
+    end
   end
 end
