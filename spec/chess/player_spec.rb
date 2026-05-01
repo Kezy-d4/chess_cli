878aefb18e320fe87bb1a9c1fde5e325be204e65
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-05-01 17:57:36 -0400

    docs: prepend legal notice to source files

--- spec/chess/player_spec.rb
+++ spec/chess/player_spec.rb
@@ -1,3 +1,7 @@
+# Chess CLI  Copyright (C) 2026  Kezy-d4
+# This software is licensed under the GNU GPLv3.
+# See the full license: https://www.gnu.org/licenses/gpl-3.0.en.html
+
 # frozen_string_literal: true
 
 describe Chess::Player do
